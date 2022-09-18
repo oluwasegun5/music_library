3f1b4bd684adeb8c6beb0fc998c5df8fe3d56989
oluwasegun5 <adeayosegun5@gmail.com>
2022-09-18 16:14:06 +0100

    initial commit

--- music_library/settings.py
+++ music_library/settings.py
@@ -11,16 +11,17 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
-
+import environ
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+env = environ.Env()
+environ.Env.read_env(BASE_DIR/".env")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-31m5rvs*2n!cctpk3z!$ias9@*l345gdj#4%)w@s3^ye352-k='
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
